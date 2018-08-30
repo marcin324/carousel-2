466d2d0b58786c73742d00ee0042c1ef2ed92abf
Marcin Olszowski <olszyna38@gmail.com>
2018-08-30 14:45:21 +0200

    Change sass/style.scss; index.html; js/script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
+'use strict';
+
 var flkty = new Flickity( '.main-carousel', {
 	wrapAround: true,
+	pageDots: false, // Ten sam efekt można uzyskać w pliku stylów dla kropek 'display: none;'
+	hash: true, // 
+	/*flkty.next(); // przycisk 'restart'
+    flkty.select(0); // przycisk 'restart'*/
 	cellAlign: 'left',
 	contain: true
+});
+
+document.getElementById('restart').addEventListener('click', function() {
+    flkty.next(); // przycisk 'restart'
+    flkty.select(0);
 });
